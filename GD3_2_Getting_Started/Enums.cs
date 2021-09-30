204476fede9b56e8a864939097d89dfa7b6c4c31
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-09-30 11:59:54 +0100

    Added enum and struct examples

---
+++ GD3_2_Getting_Started/Enums.cs
@@ -0,0 +1,26 @@
+﻿//sniper, mage, hunter, thief
+//firstperson,thirdperson,static,flight,security
+//an enum is a user-defined value type with a fixed number of
+//states or values (e.g. mage, flight)
+namespace GDLibrary.Enums //Types
+{
+    /// <summary>
+    /// Enum indicating possible player types
+    /// </summary>
+    /// <example>
+    /// Player p = new Player(PlayerType.Sniper, "", 1, 100);
+    /// </example>
+    public enum PlayerType : sbyte   //byte has range -127-> 0 -> 127
+    {
+       // internal representation is as an integer i.e. 0,1,2,3
+        Sniper, Mage, Hunter, Thief
+    }
+
+    public enum CameraType : sbyte
+    {
+        FirstPerson, ThirdPerson, 
+        Static, Flight, God, Security, 
+        Rail, Curve
+    }
+
+}
---
+++ GD3_2_Getting_Started/Point2D.cs
@@ -0,0 +1,36 @@
+﻿//x, y position, integers
+
+namespace  GDLibrary.Types
+{
+    public struct Point2D
+    {
+        #region Member Variables
+        private int x; //age
+        private int y;
+        #endregion
+
+        //property (getter, setter)
+        #region Properties
+        public int X //Age
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value >= 0 ? value : 0; //ternary operator
+
+                if (value >= 0)
+                {
+                    this.x = value;
+                }
+                else
+                {
+                    this.x = 0;
+                }
+            }
+        } 
+        #endregion
+    }
+}
