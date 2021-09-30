1a294ab8349ba464e9be07fd40078a590ff92773
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-09-30 10:03:45 +0100

    Add project files.

---
+++ GD3_2_Getting_Started/MathUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GDLibrary.Utility
+{
+    /// <summary>
+    /// Provide math functions for...game
+    /// </summary>
+    /// <see cref="GDLibrary.Program"/>
+    /// <seealso cref="GDLibrary.Program"/>
+    /// <example>
+    /// int rand = MathUtility.GetRandInRange(1, 100);
+    /// </example>
+    class MathUtility
+    {
+        /// <summary>
+        /// Returns an int in the range lo - hi
+        /// </summary>
+        /// <param name="lo">Integer value <=hi</param>
+        /// <param name="hi">Integer value >= lo</param>
+        /// <returns>Random integer</returns>
+        public static int GetRandInRange(int lo, int hi)
+        {
+            if ((hi == lo) || (hi < lo))
+                throw new ArgumentException("hi must be >= to lo");
+
+            Random rand = new Random();
+            return rand.Next(lo, hi);
+        }
+
+        //  (-10, 10, 0)
+        /// <summary>
+        /// Returns an int in the range lo - hi, excluding excl value
+        /// </summary>
+        /// <param name="lo">Integer value <=hi</param>
+        /// <param name="hi">Integer value >= lo</param>
+        /// <param name="excl">Integer value which will be excluded from lo-hi range</param>
+        /// <returns>Random integer</returns>
+        public static int RandInRangeExcl(int lo, int hi, int excl)
+        {
+            throw new NotImplementedException("It's end of class! Do next class!");
+        }
+
+
+    }
+}
---
+++ GD3_2_Getting_Started/Program.cs
@@ -0,0 +1,67 @@
+﻿/// <summary>
+/// Project to demonstrate core C# concepts created in Week 2 including:]
+/// 1) What is a namespace?
+/// 2) Static (class) vs non-static (instance) methods
+/// 3) Creating a MathUtility
+/// 4) Using Random() to generate a GetRandInRange
+/// 5) Solving get GetRandInRangeExcl
+///
+/// 
+/// </summary>
+/// 
+using GDLibrary.Utility;
+using System;
+
+namespace GDLibrary
+{
+    class Program
+    {
+
+        public void GetIt() => Console.WriteLine("sdsdfsfd");
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Start();
+        }
+
+        void Start() //non-static or instance method
+        {
+            #region Print Demo
+
+            //value types (pass by copy)
+            int x = 10;
+            double y = 20;
+            Print(x, y);
+            Console.WriteLine($"{x} and {y}");
+
+            #endregion
+
+
+            #region GetRandInRange Demo
+            for (int i = 1; i <= 10; i++)
+            {
+                int randNum = MathUtility.GetRandInRange(5, 10);
+                Console.WriteLine(randNum);
+            } 
+            #endregion
+
+        }
+
+        /// <summary>
+        /// Simple method to demonstrate that value types are passed by copy
+        /// and so changing the value INSIDE the method does not affect the variable
+        /// values passed from OUTSIDE the method
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        void Print(int a, double b)
+        {
+            a = -10;
+            b = 1E6; //1000000
+            Console.WriteLine($"{a} and {b}");
+            //Console.WriteLine(a + " and " + b);
+        }
+    }
+}
