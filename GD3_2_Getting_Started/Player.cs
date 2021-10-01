a68adad51190e22bcfbdeec17fcb908ee5c0983b
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-01 12:52:54 +0100

    Added requirements for Player

--- GD3_2_Getting_Started/Player.cs
+++ GD3_2_Getting_Started/Player.cs
@@ -1,6 +1,38 @@
-﻿namespace GDLibrary
+﻿using System.Numerics;
+using GDLibrary.Types;
+
+namespace GDLibrary
 {
-    public class Player
+    public class Player //aggregate objects => "has-a"
     {
+        private string name, texture;
+        private Integer3 translation;
+        private Vector3 rotation, scale;
+
+        //properties
+        public string Name { get; set; }
+        public string Texture { get; set; }
+
+        public Integer3 Translation
+        {
+            get
+            {
+                return this.translation;
+            }
+            set
+            {
+                this.translation = value;
+            }
+        }
+
+        //constructors - call one constructor from another (hint: this)
+        //default
+        //name, texture, set all to most appropriate
+        //full
+
+        //ToString
+
+        //Clone - shallow
+        //Clone - Deep
     }
 }
